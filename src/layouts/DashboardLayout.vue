9c98967ff9ab20f34fff80d11299362d90fa0084
unknown <lytanghaii@gmail.com>
2026-03-14 14:26:45 +0700

    add cron

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -54,6 +54,10 @@
                                 <!-- <span class="menu-icon">⚙️</span> -->
                                 <span class="menu-text">Configuration</span>
                             </RouterLink>
+                             <RouterLink @click="closeSidebar" to="/cron">
+                                <!-- <span class="menu-icon">⚙️</span> -->
+                                <span class="menu-text">Cron</span>
+                            </RouterLink>
                         </div>
                     </div>
                 </nav>
